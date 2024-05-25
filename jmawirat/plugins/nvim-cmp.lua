3d3c296c654ce41feaaff213a7af72b57db2b361
jmawirat <jmawirat@outlook.com>
2024-05-25 12:54:07 +0800

    Added LuaSnip

--- jmawirat/plugins/nvim-cmp.lua
+++ jmawirat/plugins/nvim-cmp.lua
@@ -1,23 +1,27 @@
 local M = {
 	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-nvim-lua",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
+		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 	},
 }
 
 M.config = function()
 	local cmp = require("cmp")
-	vim.opt.completeopt = { "menu", "menuone", "noselect" }
+	local luasnip = require("luasnip")
+
+	luasnip.config.setup {}
 
 	cmp.setup({
 		snippet = {
 			expand = function(args)
-				require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				luasnip.lsp_expand(args.body)
 			end,
 		},
 		window = {
@@ -25,16 +29,29 @@ M.config = function()
 			-- documentation = cmp.config.window.bordered(),
 		},
 		mapping = cmp.mapping.preset.insert({
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			['<C-j>'] = cmp.mapping.select_next_item(),
+			['<C-k>'] = cmp.mapping.select_prev_item(),
+			['<C-b>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+			['<C-Space>'] = cmp.mapping.complete(),
+			['<C-e>'] = cmp.mapping.abort(),
+			['<Enter>'] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					local selected_entry = cmp.get_selected_entry()
+					if selected_entry then
+						cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+					else
+						cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+					end
+				else
+					fallback()
+				end
+			end, { 'i', 's' }),
 		}),
 		sources = cmp.config.sources({
 			{ name = "nvim_lsp" },
 			{ name = "nvim_lua" },
-			-- { name = "orgmode" },
+			{ name = "luasnip" },
 		}, {
 			{ name = "buffer" },
 			{ name = "path" },
@@ -52,3 +69,4 @@ M.config = function()
 end
 
 return M
+
